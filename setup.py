6f1a027ceb217beaf74363299a31924619840c26
Stéphane Diemer <stephane.diemer@ubicast.eu>
2025-10-09 17:58:33 +0200

    Remove unused scripts

--- setup.py
+++ setup.py
@@ -14,6 +14,5 @@ setup(
     url='https://github.com/UbiCastTeam/metacast',
     license='GNU LGPL v2.1',
     packages=['metacast'],
-    scripts=['scripts/metacast_timeshift'],
     install_requires=['lxml'],
 )
